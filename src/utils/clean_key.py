57c497445458dded27524579303d4e2f4db47a80
Leandro Koller <lekoller@yahoo.com>
2023-09-13 00:30:18 -0300

    initial commit

---
+++ src/utils/clean_key.py
@@ -0,0 +1,33 @@
+
+def clean_key(key):
+    key = key.lower() \
+        .replace(' ', '_') \
+        .replace('/', '_') \
+        .replace('ã', 'a') \
+        .replace('á', 'a') \
+        .replace('é', 'e') \
+        .replace('í', 'i') \
+        .replace('ó', 'o') \
+        .replace('ú', 'u') \
+        .replace('ê', 'e') \
+        .replace('ô', 'o') \
+        .replace('õ', 'o') \
+        .replace('ü', 'u') \
+        .replace('â', 'a') \
+        .replace('à', 'a') \
+        .replace('í', 'i') \
+        .replace('ú', 'u') \
+        .replace('ç', 'c') \
+        .replace('(', '') \
+        .replace(')', '') \
+        .replace('º', '') \
+        .replace('ª', '') \
+        .replace('?', '') \
+        .replace('!', '') \
+        .replace(';', '') \
+        .replace(':', '') \
+        .replace(',', '') \
+        .replace('.', '') \
+        .replace(' ', '_') \
+        .replace('__', '_')
+    return key
